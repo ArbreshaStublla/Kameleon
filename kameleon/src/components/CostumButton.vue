66fe1b2616b1d08323933ad67077ea02eb45b1e6
Arbreshass <arbresha.stublla@student.uni-pr.edu>
2024-06-06 12:29:52 +0200

    button

---
+++ kameleon/src/components/CostumButton.vue
@@ -0,0 +1,42 @@
+
+<template>
+    <button class="button" @click="handleClick">
+      <span><slot></slot></span>
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CustomButton',
+    methods: {
+      handleClick(event) {
+        this.$emit('click', event);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .button {
+    display: inline-block;
+    border-radius: 4px;
+    border: none;
+    background: #1875FF;
+    color: white;
+    font-family: inherit;
+    text-align: center;
+    font-size: 13px;
+    width: 10em;
+    padding: 10px;
+    transition: all 0.4s;
+    cursor: pointer;
+  }
+  
+  .button span {
+    cursor: pointer;
+    display: inline-block;
+    position: relative;
+    transition: 0.4s;
+  }
+  </style>
+  
